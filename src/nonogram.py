b05fb2b842474bc46d6fb66afbc7905515566632
coderelated <coderelatedchannel@gmail.com>
2024-05-15 18:40:51 +0200

    small printing adjustment (but still messy code, sorry)

--- src/nonogram.py
+++ src/nonogram.py
@@ -1,4 +1,4 @@
-BLANK = ' '
+BLANK = '  '
 PRINTING_SYMBOL_MAP = {
     0 : ' ',
     1 : '•',
@@ -8,16 +8,20 @@ PRINTING_SYMBOL_MAP = {
 class Nonogram:
     def __init__(self, config):
         self.__dict__ = config
+        self.max_col_len = max(len(values) for values in self.columns)
+        self.max_row_len = max(len(values) for values in self.rows)
+        max_row_value_len = max(max(len(str(row_element)) for row_element in row) for row in self.rows)
+        max_col_value_len = max(max(len(str(col_element)) for col_element in column) for column in self.columns)
+        self.max_value_len = max([max_row_value_len, max_col_value_len])
         self.solution = [[0 for y in range(self.width)] for x in range(self.height)]
     
     def __str__(self):
-        # extend printing by title and author of the puzzle
-
-        max_col_len = max(len(values) for values in self.columns)
-        max_row_len = max(len(values) for values in self.rows)
-        
         #this is disgusting, I know
-        printing_array = [[BLANK if max_col_len > len(self.columns[j])+i or j < 0 else str(self.columns[j][i-(max_col_len-len(self.columns[j]))]) for j in range(-max_row_len, len(self.columns))] for i in range(max_col_len)]
-        printing_array.extend([[BLANK if -i > len(self.rows[j]) else PRINTING_SYMBOL_MAP.get(self.solution[j][i]) if i>=0 else str(self.rows[j][i]) for i in range(-max_row_len,self.width)] for j in range(self.height)])
+        info = [
+            f'title: {self.title}',
+            f'author: {self.by if self.by else "unknown"}'
+        ]
+        printing_array = [[BLANK if self.max_col_len > len(self.columns[j])+i or j < 0 else str(self.columns[j][i-(self.max_col_len-len(self.columns[j]))]).ljust(self.max_value_len, ' ') for j in range(-self.max_row_len, len(self.columns))] for i in range(self.max_col_len)]
+        printing_array.extend([[BLANK if -i > len(self.rows[j]) else PRINTING_SYMBOL_MAP.get(self.solution[j][i]).ljust(self.max_value_len, ' ') if i>=0 else str(self.rows[j][i]).ljust(self.max_value_len, ' ') for i in range(-self.max_row_len,self.width)] for j in range(self.height)])
 
-        return '\n'.join([' '.join(row) for row in printing_array])
+        return '\n'.join(info) + '\n'.join([' '.join(row) for row in printing_array])
